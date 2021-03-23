2e55d0c9cb1c2f82bf18b60b0c64f343bd7d770f
Luka Isailovic <lukaisailovic@gmail.com>
2021-03-24 00:13:27 +0100

    Not found request also sends html

--- src/main/java/http/response/NotFoundResponse.java
+++ src/main/java/http/response/NotFoundResponse.java
@@ -3,6 +3,8 @@ package http.response;
 public class NotFoundResponse extends Response{
     @Override
     public String getResponseString() {
-        return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n";
+        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+        response += "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Not found</title></head><body>Requested route is not found</body></html>";
+        return response;
     }
 }
