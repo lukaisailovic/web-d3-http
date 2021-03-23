5b4934d0011675c17b80a0f920bbc492914629a3
Luka Isailovic <lukaisailovic@gmail.com>
2021-03-23 23:46:06 +0100

    Remove all caps from Not found response

--- src/main/java/http/response/NotFoundResponse.java
+++ src/main/java/http/response/NotFoundResponse.java
@@ -3,6 +3,6 @@ package http.response;
 public class NotFoundResponse extends Response{
     @Override
     public String getResponseString() {
-        return "HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n";
+        return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n";
     }
 }
